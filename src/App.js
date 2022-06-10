525422bd88b5244d873c8ed23b7fea6327c66311
JubayerDev <ajashes858@gmail.com>
2022-06-10 22:29:36 +0600

    navbar, navlink, darkmode added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Navbar >
+        <Routes>
+          <Route path='/' element={ <Home />} />
+          <Route path='/about' element={ <About />} />
+          <Route path='/services' element={ <Services />} />
+          <Route path='/Contact' element={ <Contact />} />
+          <Route path='/login' element={ <Login />} />
+        </Routes>
+      </Navbar>
+    </>
   );
 }
 
